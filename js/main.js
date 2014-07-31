9728442a9dab96601ec16a1b5b206be501f252dd
Steven Deeds <stevendeeds@Stevens-Mac-mini.local>
2014-07-31 09:40:59 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+	var browserHeight = $(window).height();	
+	$("header").css({"height": browserHeight});
+});
+$("#nebulainfo").click(function() {
+	$("#nebula").fadeIn();
+});
+
+$(".closebtn").click(function() {
+	$(".modalgroup").fadeOut();
+});
+$("#claimstlinfo").click(function() {
+	$("#claimstl").fadeIn();
+});
+
+$(".closebtn").click(function() {
+	$(".modalgroup").fadeOut();
+});
+$("#lab1500info").click(function() {
+	$("#lab1500").fadeIn();
+});
+
+$(".closebtn").click(function() {
+	$(".modalgroup").fadeOut();
+});
+$("#techartistainfo").click(function() {
+	$("#techartista").fadeIn();
+});
+
+$(".closebtn").click(function() {
+	$(".modalgroup").fadeOut();
+});
+$("#thehiveinfo").click(function() {
+	$("#thehive").fadeIn();
+});
+
+$(".closebtn").click(function() {
+	$(".modalgroup").fadeOut();
+});
+
+      
